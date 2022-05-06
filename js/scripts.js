cd88636ce61140c5d38c5a72c55786fca7d329be
vinicius <v.nunhez27@gmail.com>
2022-05-06 01:56:50 -0300

    JS : Adicionar filtro ( Jquery )

--- js/scripts.js
+++ js/scripts.js
@@ -105,7 +105,47 @@ setTimeout(function(){
    $('#data-area').parallax({imageSrc: '/assets/cidadeparallax.png' })
    $('#apply-area').parallax({imageSrc: '/assets/pattern.png' })    
 
-},250)
+},200)
 
+
+// filtro do Nosso projetos 
+
+$('.filter-btn').on('click', function(){
+
+ let type = $(this).attr('id');
+ let boxes = $('.project-box');
+
+$('.main-btn').removeClass('active');
+$(this).addClass('active');
+
+
+if (type == 'dsg-btn') { 
+    eachBoxes('dsg', boxes)
+} else if ( type == 'dev-btn') {
+    eachBoxes ('dev', boxes);
+} else if ( type == 'seo-btn') { 
+    eachBoxes('seo', boxes);
+} else {
+    eachBoxes('all', boxes)
+}
+   
 
 });
+
+function eachBoxes (type, boxes) {
+   
+    if ( type == 'all'){ 
+       $(boxes).fadeIn();
+    } else { 
+        $(boxes).each(function(){
+           if(!$(this).hasClass(type)) {
+               $(this).fadeOut('slow');
+           } else { 
+              $(this).fadeIn(); 
+           }  
+        });
+    }
+
+}
+
+})
